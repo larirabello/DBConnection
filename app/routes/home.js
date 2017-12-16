0378e61b9187ce87de1be8aafe98f2134f8a2913
Larissa Rabello <lari.rabello@gmail.com>
2017-12-16 01:47:19 -0200

    Alterando conexão

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,188 +3,191 @@
  */
 //let api = require('../api/api');
 let fs = require('fs');
-module.exports = function(app){
-let errorMsg = require('./errorMsg.json');
+module.exports = function(app) {
+  let errorMsg = require('./errorMsg.json');
 
-// ---------------------- INICIO CONSULTAR -----------------
-    // ---------------------- INICIO CONSULTAR GERAL -----------------
-    app.get('/vendas/consultar', (req, res) => {
-        let resultsJson = '';
-        let connection = app.infra.connectionFactory();
-        let listas = new app.infra.applicationDAO(connection); //applicationDAO em minusculo
-        listas.lista(function (error, results, fields) {
-            fs.writeFile('bd.json', resultsJson, (err) => {
-                if(err) throw err;
-            console.log('Salvo');
-        });
-            if(results.length === 0){
-                res.send(errorMsg["consultar"][0]);
-            } else {
-                res.send(results);
-            }
-        });
-        connection.end();
-        });
-    // ---------------------- FIM CONSULTAR GERAL -----------------
-
-    // ---------------------- INICIO CONSULTAR VENDA -----------------
-    app.get('/vendas/consultar/idVenda/:idVenda', (req, res) => {
-        let idVenda= req.params.idVenda;
-    if(isNaN(idVenda)){
-        res.send(errorMsg["consultar"][5]);
-    } else {
-        console.log('~>idVenda: ', idVenda);
-        let resultJson = '';
-        let connection = app.infra.connectionFactory();
-        let lstEspecifica = new app.infra.applicationDAO(connection);
-        lstEspecifica.vendaEspecifica(idVenda, function (err, results) {
-            console.log(results.length);
-            if (results.length === 0) {
-                res.send(errorMsg["consultar"][1]);
-            } else {
-                res.send(results);
-            }
-        });
-        connection.end();
-    }
+  // ---------------------- INICIO CONSULTAR -----------------
+  // ---------------------- INICIO CONSULTAR GERAL -----------------
+  app.get('/vendas/consultar', (req, res) => {
+    let resultsJson = '';
+    let connection = app.infra.connectionFactory();
+    let listas = new app.infra.applicationDAO(connection); //applicationDAO em minusculo
+    listas.lista(function(error, results, fields) {
+      fs.writeFile('bd.json', resultsJson, err => {
+        if (err) throw err;
+        console.log('Salvo');
+      });
+      if (results.length === 0) {
+        res.send(errorMsg['consultar'][0]);
+      } else {
+        res.send(results);
+      }
+      connection.end();
     });
-    // ---------------------- FIM CONSULTAR USUARIO -----------------
+  });
+  // ---------------------- FIM CONSULTAR GERAL -----------------
 
-    // ---------------------- INICIO CONSULTAR PRODUTO -----------------
-    app.get('/vendas/consultar/idProduto/:idProduto', (req, res) => {
-        let idProduto= req.params.idProduto;
-    if(isNaN(idProduto)){
-        res.send(errorMsg["consultar"][5]);
+  // ---------------------- INICIO CONSULTAR VENDA -----------------
+  app.get('/vendas/consultar/idVenda/:idVenda', (req, res) => {
+    let idVenda = req.params.idVenda;
+    if (isNaN(idVenda)) {
+      res.send(errorMsg['consultar'][5]);
     } else {
-        let resultJson = '';
-        let connection = app.infra.connectionFactory();
-        let lstEspecifica = new app.infra.applicationDAO(connection);
-        lstEspecifica.produtoEspecifico(idProduto, function (err, results) {
-            console.log(results.length);
-            if (results.length === 0) {
-                res.send(errorMsg["consultar"][2]);
-            } else {
-                res.send(results);
-            }
-        });
-        connection.end();
-    }
-    });
-    // ---------------------- FIM CONSULTAR PRODUTO -----------------
-
-    // ---------------------- INICIO CONSULTAR USUARIO -----------------
-    app.get('/vendas/consultar/idUsuario/:idUsuario', (req, res) => {
-        let idUsuario = req.params.idUsuario;
-        if(isNaN(idUsuario)){
-            res.send(errorMsg["consultar"][5]);
+      console.log('~>idVenda: ', idVenda);
+      let resultJson = '';
+      let connection = app.infra.connectionFactory();
+      let lstEspecifica = new app.infra.applicationDAO(connection);
+      lstEspecifica.vendaEspecifica(idVenda, function(err, results) {
+        console.log(results.length);
+        if (results.length === 0) {
+          res.send(errorMsg['consultar'][1]);
         } else {
-            let resultJson = '';
-            let connection = app.infra.connectionFactory();
-            let lstEspecifica = new app.infra.applicationDAO(connection);
-            lstEspecifica.usuarioEspecifico(idUsuario, function (err, results) {
-                console.log(results);
-                if (results.length === 0) {
-                    res.send(errorMsg["consultar"][4]);
-                } else {
-                    res.send(results);
-                }
-            });
-            connection.end();
+          res.send(results);
         }
-    });
-    // ---------------------- FIM CONSULTAR USUARIO -----------------
+        connection.end();
+      });
+    }
+  });
+  // ---------------------- FIM CONSULTAR USUARIO -----------------
 
-    // ---------------------- INICIO CONSULTAR DATA -----------------
-    app.get('/vendas/consultar/dataVenda/:dataVenda', (req, res) => {
-        var dtVenda = req.params.dataVenda;
+  // ---------------------- INICIO CONSULTAR PRODUTO -----------------
+  app.get('/vendas/consultar/idProduto/:idProduto', (req, res) => {
+    let idProduto = req.params.idProduto;
+    if (isNaN(idProduto)) {
+      res.send(errorMsg['consultar'][5]);
+    } else {
+      let resultJson = '';
+      let connection = app.infra.connectionFactory();
+      let lstEspecifica = new app.infra.applicationDAO(connection);
+      lstEspecifica.produtoEspecifico(idProduto, function(err, results) {
+        console.log(results.length);
+        if (results.length === 0) {
+          res.send(errorMsg['consultar'][2]);
+        } else {
+          res.send(results);
+        }
+        connection.end();
+      });
+    }
+  });
+  // ---------------------- FIM CONSULTAR PRODUTO -----------------
+
+  // ---------------------- INICIO CONSULTAR USUARIO -----------------
+  app.get('/vendas/consultar/idUsuario/:idUsuario', (req, res) => {
+    let idUsuario = req.params.idUsuario;
+    if (isNaN(idUsuario)) {
+      res.send(errorMsg['consultar'][5]);
+    } else {
+      let resultJson = '';
+      let connection = app.infra.connectionFactory();
+      let lstEspecifica = new app.infra.applicationDAO(connection);
+      lstEspecifica.usuarioEspecifico(idUsuario, function(err, results) {
+        console.log(results);
+        if (results.length === 0) {
+          res.send(errorMsg['consultar'][4]);
+        } else {
+          res.send(results);
+        }
+        connection.end();
+      });
+    }
+  });
+  // ---------------------- FIM CONSULTAR USUARIO -----------------
+
+  // ---------------------- INICIO CONSULTAR DATA -----------------
+  app.get('/vendas/consultar/dataVenda/:dataVenda', (req, res) => {
+    var dtVenda = req.params.dataVenda;
     console.log(dtVenda);
     let resultJson = '';
     let connection = app.infra.connectionFactory();
     let lstEspecifica = new app.infra.applicationDAO(connection);
-    lstEspecifica.dataEspecifica(dtVenda, function (err, results) {
-        if(results.length === 0){
-            res.send(errorMsg["consultar"][3]);
+    lstEspecifica.dataEspecifica(dtVenda, function(err, results) {
+      if (results.length === 0) {
+        res.send(errorMsg['consultar'][3]);
+      } else {
+        res.send(results);
+      }
+      connection.end();
+    });
+  });
+  // ---------------------- FIM CONSULTAR DATA -----------------
+  // ---------------------- FIM CONSULTAR -----------------
+
+  // ---------------------- INICIO INSERIR -----------------
+  app.post('/vendas/incluir', (req, res) => {
+    let venda = req.body;
+    if (
+      isNaN(venda['idProduto']) ||
+      isNaN(venda['idUsuario']) ||
+      typeof venda['dtVenda'] !== 'string' ||
+      typeof venda['statusVenda'] !== 'string' ||
+      venda['dtVenda'].length !== 10
+    ) {
+      res.send(errorMsg['inserir'][1]);
+    } else {
+      let resultsJson = '';
+      let connection = app.infra.connectionFactory();
+      let inserir = new app.infra.applicationDAO(connection); //applicationDAO em minusculo
+      inserir.salva(venda, function(err, results) {
+        if (results['warningCount'] !== 0) {
+          res.send(errorMsg['inserir'][0]);
         } else {
-            res.send(results);
+          res.status(200).send(errorMsg['inserir'][0]);
         }
-    });
-    connection.end();
-    });
-    // ---------------------- FIM CONSULTAR DATA -----------------
-// ---------------------- FIM CONSULTAR -----------------
+        connection.end();
+      });
+    }
+  });
+  // ---------------------- FIM INSERIR -----------------
 
-// ---------------------- INICIO INSERIR -----------------
-    app.post('/vendas/incluir',(req, res) => {
-        let venda = req.body;
-        if(isNaN(venda["idProduto"])
-            || isNaN(venda["idUsuario"])
-            || typeof venda["dtVenda"] !== 'string'
-            || typeof venda["statusVenda"] !== 'string'
-            || venda["dtVenda"].length !== 10){
-                res.send(errorMsg["inserir"][1]);
-        } else {
-            let resultsJson = '';
-            let connection = app.infra.connectionFactory();
-            let inserir = new app.infra.applicationDAO(connection); //applicationDAO em minusculo
-            inserir.salva(venda, function (err, results) {
-                if (results["warningCount"] !== 0) {
-                    res.send(errorMsg["inserir"][0]);
-                } else {
-                    res.status(200).send(errorMsg['inserir'][0]);
-                }
-            });
-            connection.end();
-        }
-    });
-// ---------------------- FIM INSERIR -----------------
+  // ---------------------- INICIO ATUALIZAR -----------------
+  app.put('/vendas/editar', (req, res) => {
+    let venda = req.body;
+    console.log('REQ.BODY: ', venda);
+    let info = venda['info'];
 
-// ---------------------- INICIO ATUALIZAR -----------------
-    app.put('/vendas/editar',(req, res) => {
-        let venda = req.body;
-        console.log('REQ.BODY: ', venda);
-        let info = venda["info"];
+    console.log('venda["info"]: ', venda['info']);
+    let id = venda['id'];
+    console.log('venda["id"]: ', venda['id']);
 
-        console.log('venda["info"]: ', venda["info"]);
-        let id = venda["id"];
-        console.log('venda["id"]: ', venda["id"]);
+    if (
+      isNaN(venda['id']) ||
+      isNaN(info['idProduto']) ||
+      isNaN(info['idUsuario']) ||
+      typeof info['dtVenda'] !== 'string' ||
+      typeof info['statusVenda'] !== 'string' ||
+      info['dtVenda'].length !== 10
+    ) {
+      res.send(errorMsg['alterar'][1]);
+    } else {
+      let resultJson = '';
+      let connection = app.infra.connectionFactory();
+      let atualizar = new app.infra.applicationDAO(connection);
+      atualizar.atualiza(info, id, function(err, results) {
+        res.send(errorMsg['alterar'][0]);
+        connection.end();
+      });
+    }
+  });
+  // ---------------------- FIM ATUALIZAR -----------------
 
-        if(isNaN(venda["id"])
-            || isNaN(info["idProduto"])
-            || isNaN(info["idUsuario"])
-            || typeof info["dtVenda"] !== 'string'
-            || typeof info["statusVenda"] !== 'string'
-            || info["dtVenda"].length !== 10){
-                res.send(errorMsg["alterar"][1]);
-        } else {
-            let resultJson = '';
-            let connection = app.infra.connectionFactory();
-            let atualizar = new app.infra.applicationDAO(connection);
-            atualizar.atualiza(info, id, function (err, results) {
-                res.send(errorMsg['alterar'][0]);
-            });
-            connection.end();
-        }
-    });
-// ---------------------- FIM ATUALIZAR -----------------
+  // ---------------------- INICIO DELETAR -----------------
+  app.delete('/vendas/excluir/', (req, res) => {
+    let idVendas = req.body.idVendas;
+    console.log(idVendas);
+    if (isNaN(idVendas)) {
+      res.send(errorMsg['excluir'][1]);
+    } else {
+      console.log(idVendas);
+      let resultJson = '';
+      let connection = app.infra.connectionFactory();
+      let deletar = new app.infra.applicationDAO(connection);
+      deletar.deleta(idVendas, function(err, results) {
+        res.send(errorMsg['excluir'][0]);
+        connection.end();
+      });
+    }
+  });
 
-// ---------------------- INICIO DELETAR -----------------
-    app.delete('/vendas/excluir/', (req,res) => {
-        let idVendas = req.body.idVendas;
-        console.log(idVendas);
-        if (isNaN(idVendas)) {
-            res.send(errorMsg["excluir"][1]);
-        } else {
-
-            console.log(idVendas);
-            let resultJson = '';
-            let connection = app.infra.connectionFactory();
-            let deletar = new app.infra.applicationDAO(connection);
-            deletar.deleta(idVendas, function (err, results) {
-                res.send(errorMsg["excluir"][0]);
-            });
-            connection.end();
-        }
-    });
-
-// ---------------------- FIM DELETAR -----------------
+  // ---------------------- FIM DELETAR -----------------
 };
